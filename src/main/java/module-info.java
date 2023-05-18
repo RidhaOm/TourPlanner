1454b22158e9418cd0ab2cb8347a4581466030c8
Maja <maja1cvetkovska@gmail.com>
2023-05-18 20:50:38 +0200

    Tour and TourLog classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.tourplanner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
+    requires com.fasterxml.jackson.annotation;
+    requires opencsv;
 
 
     opens com.example.tourplanner to javafx.fxml;
