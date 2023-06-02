e81133dd6feb130026adc7d020202f48b7bf0550
Maja <maja1cvetkovska@gmail.com>
2023-06-02 23:13:35 +0200

    fixed the error in ViewFactory

--- src/main/java/at/technikum/tourplanner/view/ViewFactory.java
+++ src/main/java/at/technikum/tourplanner/view/ViewFactory.java
@@ -27,9 +27,22 @@ public class ViewFactory {
         if (viewClass == MainView.class) {
             return new MainView();
         }
+        if (viewClass == NavigationBarView.class) {
+            return new NavigationBarView();
+        }
+        if (viewClass == SearchBarView.class) {
+            return new SearchBarView();
+        }
+        if (viewClass == TourDetailsView.class) {
+            return new TourDetailsView();
+        }
+        if (viewClass == TourLogView.class) {
+            return new TourLogView();
+        }
 
         throw new IllegalArgumentException();
     }
+
 
     public static ViewFactory getInstance() {
         if (null == instance) {
